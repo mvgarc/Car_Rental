27b12b53eefd4892ad5efedcbe0890148425c510
María García <mariav2601@gmail.com>
2024-11-05 15:43:27 -0400

    Crea una instancia de usuario

--- src/main.py
+++ src/main.py
@@ -1,6 +1,9 @@
 from rental_car import RentalCar
 from user import User
 
+# Crear instancias de carros
 car1 = RentalCar("Toyota Corolla", 50, "Toyota")
 car2 = RentalCar("Ford Fiesta", 40, "Ford")
 car3 = RentalCar("Honda Civic", 60, "Honda")
+
+user1 = User("Maria", 1)
