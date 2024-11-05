69c0f03a8ec70741eb33550ae8c03a8ffedb84c1
María García <mariav2601@gmail.com>
2024-11-05 15:42:41 -0400

    se crea la instancia de carros

--- src/main.py
+++ src/main.py
@@ -1,2 +1,6 @@
 from rental_car import RentalCar
 from user import User
+
+car1 = RentalCar("Toyota Corolla", 50, "Toyota")
+car2 = RentalCar("Ford Fiesta", 40, "Ford")
+car3 = RentalCar("Honda Civic", 60, "Honda")
