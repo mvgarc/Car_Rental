7fd1407e95e69a0d10c3bfa928d5a875ca53b0b9
María García <mariav2601@gmail.com>
2024-11-05 15:32:56 -0400

    creo la clase RentalCar y del constructor

---
+++ src/rental_car.py
@@ -0,0 +1,6 @@
+class RentalCar:
+    def __init__(self, car,price,brand):
+        self.car = car
+        self.price = price
+        self.brand = brand
+        self.available = True # Indica si el carro está disponible para alquilar
