2ceadd314d4b0f26c36b2943a4b1ff1922f42567
María García <mariav2601@gmail.com>
2024-11-05 15:07:33 -0400

    añadimos comentarios del contructor

--- sistema_alquiler_carros/src/rental_car.py
+++ sistema_alquiler_carros/src/rental_car.py
@@ -1,4 +1,10 @@
 class User:
     def __init__(self,name,user_id):
+        """
+        Inicializa un objeto User con un nombre y un ID de usuario.
+        
+        :param name: Nombre del usuario
+        :param user_id: ID único del usuario
+        """
         self.name = name
         self.user_id = user_id
