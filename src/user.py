7639de680da1bf6a7b674a46c8e8440e61ec337f
María García <mariav2601@gmail.com>
2024-11-05 15:23:27 -0400

    creacion de la clase user y constructor

---
+++ src/user.py
@@ -0,0 +1,11 @@
+class User:
+    def __init__(self,name,user_id):
+        """
+        Inicializa un objeto User con un nombre y un ID de usuario.
+        
+        :param name: Nombre del usuario
+        :param user_id: ID único del usuario
+        """
+        self.name = name
+        self.user_id = user_id
+        self.borrowed_cars = [] # Lista para rastrear los carros alquilados por el usuario
