b8dde65c9e996ffbdc3bdfe00ef45993308d054f
María García <mariav2601@gmail.com>
2024-11-05 15:33:40 -0400

    se añade comentarios

--- src/rental_car.py
+++ src/rental_car.py
@@ -1,5 +1,12 @@
 class RentalCar:
     def __init__(self, car,price,brand):
+        """
+        Inicializa un objeto RentalCar con su nombre, precio y marca.
+        
+        :param car: Nombre del carro
+        :param price: Precio de alquiler por día
+        :param brand: Marca del carro
+        """
         self.car = car
         self.price = price
         self.brand = brand
